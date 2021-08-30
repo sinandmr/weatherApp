b5671af825cc33dee174ccea3cbea86d431a4dbe
Sinan Demir <sinanndmrr@gmail.com>
2021-08-30 17:53:05 +0300

    dom elements are selected.

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+const searchInput = document.querySelector('#search');
+const searchBtn = document.querySelector('.choose-city');
+const tempText = document.querySelector('.temp-info');
+const cloudImage = document.querySelector('.weather-image');
+const weatherInfo = document.querySelector('.weather-info');
+const cityName = document.querySelector('.city-info');
+const apiKey = '41359b20d208e746135272a023315b3b';
+const days = document.querySelectorAll('.day');
