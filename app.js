7a3363d061616eeea17e026ba8b82aca82c3d82c
Sinan Demir <sinanndmrr@gmail.com>
2021-08-31 13:04:16 +0300

    Promise structures are arranged async

--- app.js
+++ app.js
@@ -21,49 +21,40 @@ navigator.geolocation.getCurrentPosition(function (position) {
   fetchLocationName(lat, lon);
 });
 
-async function getUserWeatherData(lat, lon) {
-  const data = await fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${apiKey}`
-  )
-    .then(res => res.json())
-    .then(data => {
-      // Current day data
-      tempText.textContent = kelvintoCelsius(data.current.temp) + '°C';
-      weatherInfo.textContent = data.current.weather[0].description;
-      cloudImage.src = `src/png/${data.current.weather[0].main}.png`;
+const getUserWeatherData = async (lat, lon) => {
+  const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${apiKey}`;
+  const fetchData = await fetch(apiURL);
+  const data = await fetchData.json();
 
-      // Week data
-      days.forEach((day, index) => {
-        day.querySelector(
-          '.day-photo'
-        ).src = `src/png/${data.daily[index].weather[0].main}.png`;
-        day.querySelector('.day-temp').textContent = `${kelvintoCelsius(
-          data.daily[index].temp.min
-        )}/${kelvintoCelsius(data.daily[index].temp.max)}`;
-      });
-    });
-}
+  tempText.textContent = kelvintoCelsius(data.current.temp) + '°C';
+  weatherInfo.textContent = data.current.weather[0].description;
+  cloudImage.src = `src/png/${data.current.weather[0].main}.png`;
+
+  // Week data
+  days.forEach((day, index) => {
+    day.querySelector(
+      '.day-photo'
+    ).src = `src/png/${data.daily[index].weather[0].main}.png`;
+    day.querySelector('.day-temp').textContent = `${kelvintoCelsius(
+      data.daily[index].temp.min
+    )}/${kelvintoCelsius(data.daily[index].temp.max)}`;
+  });
+};
 
 const fetchLocationName = async (lat, lon) => {
-  await fetch(
-    `https://www.mapquestapi.com/geocoding/v1/reverse?key=${mapQuestApi}&location=${lat}%2C${lon}&outFormat=json&thumbMaps=false`
-  )
-    .then(res => res.json())
-    .then(data => {
-      cityName.textContent = data.results[0].locations[0].adminArea5;
-    });
+  const apiURL = `https://www.mapquestapi.com/geocoding/v1/reverse?key=${mapQuestApi}&location=${lat}%2C${lon}&outFormat=json&thumbMaps=false`;
+  const fetchData = await fetch(apiURL);
+  const data = await fetchData.json();
+  cityName.textContent = data.results[0].locations[0].adminArea5;
 };
 
 // if a query is made with the entered data, these parts will work.
-const getCityLocation = city => {
-  return fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-  )
-    .then(res => res.json())
-    .then(data => {
-      cityName.textContent = data.name;
-      getUserWeatherData(data.coord.lat, data.coord.lon);
-    });
+const getCityLocation = async city => {
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const fetchData = await fetch(apiURL);
+  const data = await fetchData.json();
+  cityName.textContent = data.name;
+  getUserWeatherData(data.coord.lat, data.coord.lon);
 };
 
 searchBtn.addEventListener('click', e => {
